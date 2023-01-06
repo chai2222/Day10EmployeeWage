b5ccb9fb7aecc2ac2f5c28efa2abce56470c2860
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-06 13:19:04 +0530

    MultipleCompany

--- EmployeeWageComputationProblem/EmployeeWage.cs
+++ EmployeeWageComputationProblem/EmployeeWage.cs
@@ -5,17 +5,14 @@ namespace EmployeeWageComputationProblem
     public class EmployeWage
     {
 
-        public const int IS_PART_TIME = 1;
+        public const int IS_PART_TIME = 3;
         public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAY = 2;
-        public const int MAX_HRS_IN_MONTH = 10;
-        public static int computeEmpWage()
+        public static int computrEmpWage(string company, int empRatePerHour, int numofworkingday, int maxiHoursPerMonth)
         {
-            int empHrs = 0, totalEmphrs = 0, totalWorkingDays = 0;
-            while (totalEmphrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAY)
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDAys = 0;
+            while (totalEmpHrs <= maxiHoursPerMonth && totalWorkingDAys < numofworkingday)
             {
-                totalWorkingDays++;
+                totalWorkingDAys++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -30,12 +27,12 @@ namespace EmployeeWageComputationProblem
                         empHrs = 0;
                         break;
                 }
-                totalEmphrs += empHrs;
-                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDAys + "Emp Hrs :" + empHrs);
             }
-            int totalEmpWage = totalEmphrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
-            return totalEmpWage;
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage for company : " + company + "is: " + totalEmpHrs);
+            return totalEmpHrs;
         }
 
     }
