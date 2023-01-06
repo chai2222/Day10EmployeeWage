8a3bbe13773c82616db89a1f3ad01408cd55635b
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-06 12:48:26 +0530

    EmployeePresentAndAbsent

---
+++ EmployeeWageComputationProblem/EmployeeWage.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace EmployeeWageComputationProblem
+{
+    public class EmployeWage
+    {
+        public static void EmpoyeStatus()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next();
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employe is present");
+            }
+            else
+            {
+                Console.WriteLine("Employe is absent");
+            }
+
+        }
+    }
+}
