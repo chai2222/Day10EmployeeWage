5ff5d8c16725015e71d63f0038776f0448fe2963
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-07 08:14:41 +0530

    CompanyMultiWage

--- EmployeeWageComputationProblem/Program.cs
+++ EmployeeWageComputationProblem/Program.cs
@@ -1,2 +1,16 @@
-﻿//Welcome to Employee Wage Computation Program
+﻿namespace EmployeeWageComputationProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Relience", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.ToString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.ToString());
+        }
+    }
+}
 
